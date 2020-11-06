f79a0dc3afd20dafa014a191687bf4ee42e83f53
RecclessCODer <kyochanmichael@gmail.com>
2020-11-06 13:56:26 +0900

    develop ver

--- Commonvoice_dataset_statistic.py
+++ Commonvoice_dataset_statistic.py
@@ -1,3 +1,4 @@
+import random
 
 
 dev_manifest_path = '../cv-corpus-5.1-2020-06-22/en/dev.tsv'
@@ -38,6 +39,9 @@ def count_accent(_dev_data, _train_data, _test_data, _validated_data):
 
 
 def count_speaker(manifest):
+    """
+    count number of speakers
+    """
     speaker_set = set()
     for speaker, __, __, __, __, __, __, __, __, __, in manifest:
         speaker_set.add(speaker)
@@ -52,22 +56,36 @@ def statistic_accent(_dev_data, _train_data, _test_data, _validated_data, _accen
     """
     all_data = dev_data + train_data + test_data + validated_data
     all_accent_data = []
-    accent_sentence_count = accent_dic.copy()
+    accent_sentence_count = accent_dic.copy()  # create a dict of {accents: numbers of sentences}
     for accent_name in accent_dic.keys():
         accent_sentence_count[accent_name] = 0  # initial count number
     for i in range(0, len(all_data)):
         no, no, no, no, no, no, no, a, no, no, = all_data[i]
         if a in _accent_set:
             all_accent_data.append(all_data[i])  # create a manifest with accent labels
-            accent_sentence_count[a] = accent_sentence_count[a] + 1
+            accent_sentence_count[a] = accent_sentence_count[a] + 1  # count numbers of accent sentences
     return all_accent_data, accent_sentence_count
 
 
-def spilt_manifest(all_acc_data, _accent_set): #  TODO implement
+def spilt_manifest(all_acc_data, _accent_set):
     """
     spilt data into five data, train_accent_data, dev_accent_data, test_accent_data
     test_scotland_data, test_ireland_data.
     """
+    refine_data = {}  # stored data except scotland data and ireland data
+    refine2_data = {}  # stored data
+    train_accent_data, dev_accent_data, test_accent_data = {}, {}, {}
+    test_scotland_data, test_ireland_data = {}, {}
+    for i in range(0, len(all_acc_data)):
+        no, no, no, no, no, no, no, a, no, no, = all_acc_data[i]
+        if a == 'scotland':
+            test_scotland_data.append(all_acc_data[i])
+        elif a == 'ireland':
+            test_ireland_data.append(all_acc_data[i])
+        else:
+            refine_data.append(all_acc_data[i])
+    for i in range(0, len(refine_data)):
+
 
     train_accent_data
 
@@ -82,7 +100,8 @@ validated_data = read_manifest(validated_manifest_path)
 accent_dic, accent_set = count_accent(dev_data, train_data, test_data, validated_data)  # count accents number
 print(accent_dic)
 
-new_accent_data, accent_sentence_num = statistic_accent(dev_data, train_data, test_data, validated_data, accent_set)  # count sentences for each accent
+new_accent_data, accent_sentence_num = statistic_accent(dev_data, train_data, test_data, validated_data, accent_set)
+# count sentences for each accent
 print(accent_sentence_num)
 
 
